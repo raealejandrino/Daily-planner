c4a751925220b088dbbaa27d87ecfa6eb97187fd
Rae Alejandrino <raealejandrino@gmail.com>
2021-10-19 13:00:10 -0400

    Added closest method to save click function

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,3 +1,31 @@
+
+// Set current time at top of page
 var currentTime = moment().format("dddd, MMMM Do");
 
 $("#currentDay").text(currentTime);
+
+// Edit or input a task when clicking on the text field
+
+$(".task").on("click", "p", function() {
+    var text = $(this)
+        .text()
+        .trim();
+
+    var textInput = $("<textarea>")
+        .addClass("form-control bg-transparent")
+        .val(text);
+
+    $(this).replaceWith(textInput);
+    textInput.trigger("focus");
+    console.log(text);
+});
+
+// Clicking on the save button saves the edited input
+
+$(".save").click(function() {
+    // get the textarea current value/text
+    var text = $(this)
+    .closest(".task");
+        
+    console.log(text);
+});
